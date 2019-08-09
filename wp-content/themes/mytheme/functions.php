91a444e63f027777f9cac70e56df94636ef51a8f
Asad Jakhar <rustyonrampage@hotmail.com>
2019-08-09 13:25:56 +0500

    Step 1.1: Adding the dependencies CSS & JS in Wordpress
    In this step, we will hook our CSS & JS in the wordpress.
    
    However, our assets won't show up in the wordpress just yet.

---
+++ wp-content/themes/mytheme/functions.php
@@ -0,0 +1,18 @@
+<?php
+    function add_theme_scripts(){
+        // +Stylesheets+
+        // Args{ name. path }
+        // load the main stylesheet in root folder; style.css
+        wp_enqueue_style( 'style', get_stylesheet_uri() ); 
+        // load the bootstrap stylesheet
+	    wp_enqueue_style( 'bootstrap_css', get_theme_file_uri( '/assets/css/bootstrap.min.css' ) );
+
+        // +Scripts+
+        // Args{ name. path, dependencies, in_footer }
+        // load the popper.js on which bootstrap.js depends
+        wp_enqueue_script( 'poper_js',  get_theme_file_uri( '/assets/js/popper.min.js' ), array('jquery'), true );
+        // load the bootstrap script
+        wp_enqueue_script( 'bootstrap_js',  get_theme_file_uri( '/assets/js/bootstrap.min.js' ), array('jquery', 'popper_js'), true );
+    }
+    add_action( 'wp_enqueue_scripts' , 'add_theme_scripts');
+?>
