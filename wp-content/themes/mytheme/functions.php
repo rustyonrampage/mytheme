28aebee50386397771133b9b48a21da3b446bca5
Asad Jakhar <rustyonrampage@hotmail.com>
2019-08-09 18:00:12 +0500

    Spell fix in functions.php

--- wp-content/themes/mytheme/functions.php
+++ wp-content/themes/mytheme/functions.php
@@ -12,7 +12,7 @@
         // load the popper.js on which bootstrap.js depends
         wp_enqueue_script( 'poper_js',  get_theme_file_uri( '/assets/js/popper.min.js' ), array('jquery'), true );
         // load the bootstrap script
-        wp_enqueue_script( 'bootstrap_js',  get_theme_file_uri( '/assets/js/bootstrap.min.js' ), array('jquery', 'popper_js'), true );
+        wp_enqueue_script( 'bootstrap_js',  get_theme_file_uri( '/assets/js/bootstrap.min.js' ), array('jquery', 'poper_js'), true );
     }
     add_action( 'wp_enqueue_scripts' , 'add_theme_scripts');
 ?>
