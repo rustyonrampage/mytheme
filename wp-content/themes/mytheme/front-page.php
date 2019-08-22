2713e2e4e26396ab85dc1b3667c9f2285289ad72
Asad Jakhar <rustyonrampage@hotmail.com>
2019-08-22 08:35:39 -0700

    Step 2.1: Featured Post Set
    We added functionality in our Frontpage, to display a featured post. Which has a banner image and other stuff.

--- wp-content/themes/mytheme/front-page.php
+++ wp-content/themes/mytheme/front-page.php
@@ -49,18 +49,18 @@
       if(sizeof($featured_post)>0){
 
         $rendered_feature_post['title'] = $featured_post[0]["post_title"];
-        $rendered_feature_post['text'] = $featured_post[0]["post_content"];
+        $rendered_feature_post['text'] = wp_trim_words( $featured_post[0]["post_content"], 23 );
         $rendered_feature_post['date'] = $featured_post[0]["post_date"];
         $rendered_feature_post['link'] = $featured_post[0]["guid"];
-        $photo = get_the_post_thumbnail_url( $featured_post[0]["ID"] );
+        $$rendered_feature_post['image']  = get_the_post_thumbnail_url( $featured_post[0]["ID"] );
       }
       $a="a";
     ?>
-  <div class="jumbotron p-4 p-md-5 text-white rounded bg-dark">
+  <div style="background-image:url(<?php print $$rendered_feature_post['image'] ?>)" class="jumbotron  p-4 p-md-5 text-white rounded bg-dark">
     <div class="col-md-6 px-0">
-      <h1 class="display-4 font-italic">Title of a longer featured blog post</h1>
-      <p class="lead my-3">Multiple lines of text that form the lede, informing new readers quickly and efficiently about what’s most interesting in this post’s contents.</p>
-      <p class="lead mb-0"><a href="https://getbootstrap.com/docs/4.3/examples/blog/#" class="text-white font-weight-bold">Continue reading...</a></p>
+      <h1 class="display-4 font-italic"><?php print $rendered_feature_post['title']; ?></h1>
+      <p class="lead my-3"><?php print $rendered_feature_post['text']; ?></p>
+      <p class="lead mb-0"><a href="<?php print $rendered_feature_post['link']; ?>" class="text-white font-weight-bold">Continue reading...</a></p>
     </div>
   </div>
 
