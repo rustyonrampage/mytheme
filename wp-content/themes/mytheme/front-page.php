d50db7d4ee0c670e0f532af98d7ea84f3971da75
Asad Jakhar <rustyonrampage@hotmail.com>
2019-08-23 01:05:06 -0700

    Step 2.2: Recent Posts Row Set
    Beneath the main Featured Post, there is a row of recent posts (also named as featured posts).

--- wp-content/themes/mytheme/front-page.php
+++ wp-content/themes/mytheme/front-page.php
@@ -99,7 +99,7 @@
           <a href="<?php print $item['guid']; ?>" class="stretched-link">Continue reading</a>
         </div>
         <div class="col-auto d-none d-lg-block">
-          <svg class="bd-placeholder-img" width="200" height="250" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+          <img class="img" style="object-fit: cover;" width="200" height="250"  src="<?php print get_the_post_thumbnail_url($item['ID']); ?>" alt="">
         </div>
       </div>
     </div>
