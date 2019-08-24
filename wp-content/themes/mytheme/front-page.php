51587f6fcbc1c5ada1389727a11aaf6a9f041490
Asad Jakhar <rustyonrampage@hotmail.com>
2019-08-24 00:57:00 -0700

    a push

--- wp-content/themes/mytheme/front-page.php
+++ wp-content/themes/mytheme/front-page.php
@@ -4,16 +4,37 @@
   <header class="blog-header py-3">
     <div class="row flex-nowrap justify-content-between align-items-center">
       <div class="col-4 pt-1">
-        <a class="text-muted" href="https://getbootstrap.com/docs/4.3/examples/blog/#">Subscribe</a>
+        <a class="text-muted" href="<?php print get_bloginfo('rss_url'); ?>">Subscribe</a>
       </div>
       <div class="col-4 text-center">
-        <a class="blog-header-logo text-dark" href="https://getbootstrap.com/docs/4.3/examples/blog/#">Large</a>
+        <a class="blog-header-logo text-dark" href="<?php print get_bloginfo('wpurl'); ?>"><?php print get_bloginfo('name') ; ?></a>
       </div>
       <div class="col-4 d-flex justify-content-end align-items-center">
-        <a class="text-muted" href="https://getbootstrap.com/docs/4.3/examples/blog/#">
+        
+        <!-- <a class="text-muted" href="https://getbootstrap.com/docs/4.3/examples/blog/#">
           <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="mx-3" role="img" viewBox="0 0 24 24" focusable="false"><title>Search</title><circle cx="10.5" cy="10.5" r="7.5"></circle><path d="M21 21l-5.2-5.2"></path></svg>
-        </a>
-        <a class="btn btn-sm btn-outline-secondary" href="https://getbootstrap.com/docs/4.3/examples/blog/#">Sign up</a>
+        </a> -->
+        <div class="input-group mb-3">
+          <input type="text" class="form-control" aria-label="Amount (to the nearest dollar)">
+          <div class="input-group-append">
+            <span class="input-group-text">.00</span>
+          </div>
+        </div>
+
+
+        <?php 
+          // Check if a User is logged in or not
+          $login_logout = array();
+          if( is_user_logged_in() ){
+            $login_logout['text'] = "Logout";
+            $login_logout['url'] = wp_logout_url(get_bloginfo( 'wpurl' ) );
+          }else{
+            $login_logout['text'] = "Login";
+            $login_logout['url'] = wp_login_url();
+          }
+          $a = 'a';
+        ?>
+        <a class="btn btn-sm btn-outline-secondary" href="<?php print $login_logout['url'];  ?>"><?php print $login_logout['text']; ?></a>
       </div>
     </div>
   </header>
@@ -171,7 +192,6 @@
           _e( 'Sorry, no posts matched your criteria.', 'textdomain' ); 
       endif; 
       ?>
-
     </div><!-- /.blog-main -->
 
     <aside class="col-md-4 blog-sidebar">
--- wp-content/themes/mytheme/functions.php
+++ wp-content/themes/mytheme/functions.php
@@ -16,9 +16,30 @@
     }
     add_action( 'wp_enqueue_scripts' , 'add_theme_scripts');
 
-
+    // Theme supports
 	add_theme_support( 'post-thumbnails' );
     
-
+    // CUstomizing our search form 
+    function wpdocs_my_search_formssssssss( $form ) {
+        $form = '<form role="search" method="get" id="searchform" class="searchform" action="' . home_url( '/' ) . '" >
+        <div><label class="screen-reader-text" for="s">' . __( 'Search for:' ) . '</label>
+        <input type="text" value="' . get_search_query() . '" name="s" id="s" />
+        <input type="submit" id="searchsubmit" value="'. esc_attr__( 'Search' ) .'" />
+        </div>
+        </form>';
+     
+        return $form;
+    }
+    function wpdocs_my_search_form( $form ) {
+        $form = '<form role="search" method="get" id="searchform" class="searchform" action="' . home_url( '/' ) . '" >
+        <div><label class="screen-reader-text" for="s">' . __( 'Search for:' ) . '</label>
+        <input type="text" value="' . get_search_query() . '" name="s" id="s" />
+        <input type="submit" id="searchsubmit" value="'. esc_attr__( 'Search' ) .'" />
+        </div>
+        </form>';
+     
+        return $form;
+    }
+    add_filter( 'get_search_form', 'wpdocs_my_search_form' );
 
 ?>
