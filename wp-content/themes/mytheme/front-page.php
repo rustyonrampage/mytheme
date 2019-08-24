e1cd147695a2024057ac14944cd3290e6325dd75
Asad Jakhar <rustyonrampage@hotmail.com>
2019-08-24 01:25:52 -0700

    Added Fontawesome 5

--- wp-content/themes/mytheme/front-page.php
+++ wp-content/themes/mytheme/front-page.php
@@ -9,17 +9,19 @@
       <div class="col-4 text-center">
         <a class="blog-header-logo text-dark" href="<?php print get_bloginfo('wpurl'); ?>"><?php print get_bloginfo('name') ; ?></a>
       </div>
-      <div class="col-4 d-flex justify-content-end align-items-center">
+      <div class="col-4 d-flex flex-row justify-content-end align-items-center">
         
         <!-- <a class="text-muted" href="https://getbootstrap.com/docs/4.3/examples/blog/#">
           <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="mx-3" role="img" viewBox="0 0 24 24" focusable="false"><title>Search</title><circle cx="10.5" cy="10.5" r="7.5"></circle><path d="M21 21l-5.2-5.2"></path></svg>
         </a> -->
-        <div class="input-group mb-3">
-          <input type="text" class="form-control" aria-label="Amount (to the nearest dollar)">
-          <div class="input-group-append">
-            <span class="input-group-text">.00</span>
+        <form class="mb-0 mr-3" method="GET" action="<?php print home_url('/'); ?>">
+          <div class="input-group input-group-sm">
+            <input type="text" class="form-control" aria-label="Amount (to the nearest dollar)">
+            <div class="input-group-append">
+              <button class="btn btn-outline-secondary" type="submit">Submit</button>
+            </div>
           </div>
-        </div>
+        </form>
 
 
         <?php 
--- wp-content/themes/mytheme/functions.php
+++ wp-content/themes/mytheme/functions.php
@@ -6,6 +6,8 @@
         wp_enqueue_style( 'style', get_stylesheet_uri(), array('bootstrap_css') ); 
         // load the bootstrap stylesheet
 	    wp_enqueue_style( 'bootstrap_css', get_theme_file_uri( '/assets/css/bootstrap.min.css' ) );
+        // loead the font-awesoome
+	    wp_enqueue_style( 'fontawesome_css', get_theme_file_uri( '/assets/css/fontawesome.min.css' ) );
 
         // +Scripts+
         // Args{ name. path, dependencies, in_footer }
@@ -20,16 +22,6 @@
 	add_theme_support( 'post-thumbnails' );
     
     // CUstomizing our search form 
-    function wpdocs_my_search_formssssssss( $form ) {
-        $form = '<form role="search" method="get" id="searchform" class="searchform" action="' . home_url( '/' ) . '" >
-        <div><label class="screen-reader-text" for="s">' . __( 'Search for:' ) . '</label>
-        <input type="text" value="' . get_search_query() . '" name="s" id="s" />
-        <input type="submit" id="searchsubmit" value="'. esc_attr__( 'Search' ) .'" />
-        </div>
-        </form>';
-     
-        return $form;
-    }
     function wpdocs_my_search_form( $form ) {
         $form = '<form role="search" method="get" id="searchform" class="searchform" action="' . home_url( '/' ) . '" >
         <div><label class="screen-reader-text" for="s">' . __( 'Search for:' ) . '</label>
